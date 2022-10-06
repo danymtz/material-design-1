cdfbb8766f13dfeefd89839d0adeac31e31c9961
danymtz <dany.mtz.ayala@gmail.com>
2022-10-05 19:03:01 -0500

    v0.2

--- src/app/components/shared/dialog/dialog.component.ts
+++ src/app/components/shared/dialog/dialog.component.ts
@@ -1,3 +1,4 @@
+import { JsonPipe } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
@@ -8,10 +9,29 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class DialogComponent implements OnInit {
 
+  flagAdd: boolean = true;
+  colorAdd: string = '#000000'
+
+  flagLike: boolean = true;
+  colorLike: string = '#000000'
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
-    console.log('Datos: ', this.data);
+
   }
 
+  addMovie(movie: any){
+    console.log('Movie add');
+    localStorage.setItem('movie', JSON.stringify(movie))
+    this.flagAdd = !this.flagAdd;
+    if (this.flagAdd) this.colorAdd = '#000000';
+      else this.colorAdd = 'green';    
+  }
+
+  likeMovie(){
+    this.flagLike = !this.flagLike;
+    if (this.flagLike) this.colorLike = '#000000';
+      else this.colorLike = 'green';
+  }
 }
